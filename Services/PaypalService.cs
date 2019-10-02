f792232b4ce5dc3ac0f7fb0497e2c9cf6272b4c2
“Elaine <fatima.elaine16@gmail.com>
2019-10-02 18:10:13 +0100

    Criado o projeto

---
+++ Services/PaypalService.cs
@@ -0,0 +1,18 @@
+﻿namespace ProcessamentoContrato.Services
+{
+    class PaypalService : IOnlinePaymentService
+    {
+        private double FeePercentage = 0.02;
+        private double MonthlyInterest = 0.01;
+
+        public double Interest(double amount, int months)
+        {
+            return amount * MonthlyInterest * months;
+        }
+
+        public double PaymentFee(double amount)
+        {
+            return amount * FeePercentage;
+        }
+    }
+}
